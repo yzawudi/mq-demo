36336a3d4215889d3cd54ca98ca62c748f1c35b5
yuzhouan <yuzhouan@qianxin.com>
2022-03-11 19:05:22 +0800

    first commit

---
+++ publisher/src/test/java/cn/itcast/mq/spring/SpringAmqpTest.java
@@ -0,0 +1,83 @@
+package cn.itcast.mq.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringAmqpTest {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Test
+    public void testSimpleQueue() {
+        String queueName = "simple.queue";
+        String message = "fuck you";
+        rabbitTemplate.convertAndSend(queueName, message);
+    }
+
+    /**
+     * workQueue
+     * 向队列中不停发送消息，模拟消息堆积。
+     */
+    @Test
+    public void testWorkQueue() throws InterruptedException {
+        String queueName = "simple.queue";
+        String message = "hello message_";
+        for (int i = 0; i < 50; i++) {
+            // 发送消息
+            rabbitTemplate.convertAndSend(queueName, message + i);
+            Thread.sleep(20);
+        }
+    }
+
+    @Test
+    public void testFanoutExchange() {
+        // 队列名称
+        String exchangeName = "yza.fanout";
+        // 消息
+        String message = "hello, everyone!";
+        rabbitTemplate.convertAndSend(exchangeName, "", message);
+    }
+
+    @Test
+    public void testSendDirectExchange() {
+        // 交换机名称
+        String exchangeName = "yza.direct";
+        // 消息
+        String message = "黄色 警报！日本乱排核废水，导致海洋生物变异，惊现哥斯拉！";
+        // 发送消息
+        rabbitTemplate.convertAndSend(exchangeName, "yellow", message);
+    }
+
+    /**
+     * topicExchange
+     */
+    @Test
+    public void testSendTopicExchange() {
+        // 交换机名称
+        String exchangeName = "yza.topic";
+        // 消息
+        String message = "喜报！孙悟空大战哥斯拉，胜!";
+        // 发送消息
+        rabbitTemplate.convertAndSend(exchangeName, "China.news", message);
+    }
+
+    @Test
+    public void testSendMap() {
+        // 准备消息
+        Map<String, Object> msg = new HashMap<>();
+        msg.put("name", "Jack");
+        msg.put("age", 21);
+        // 发送消息
+        rabbitTemplate.convertAndSend("yza.topic","china.news", msg);
+    }
+}
